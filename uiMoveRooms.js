b589596b94e591f1b05cce27f1a92d17d9527d83
d8a0 <151666577+d8a0@users.noreply.github.com>
2023-12-12 15:25:17 +0000

    Initial commit

---
+++ uiMoveRooms.js
@@ -0,0 +1,215 @@
+class Room {
+  constructor(name, description, ) {
+    this._name = name;
+    this._description = description;
+    this._linkedRooms = {};
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get character() {
+    return this._character
+  }
+
+  set name(value) {
+    if (value.length < 4) {
+      alert("Name is too short.");
+      return;
+    }
+    this._name = value;
+  }
+
+  set description(value) {
+    if (value.length < 4) {
+      alert("description is too short.");
+      return;
+    }
+    this._description = value;
+  }
+
+  set character(value) {
+    this._character = value;
+  }
+
+  /**
+   * a method to produce friendly room description
+   * 
+   * @returns {string} description of the room
+   * @author Neil Bizzell
+   * @version 1.0
+   */
+  describe() {
+    return "Looking around the " + this._name + " you can see " + this._description;
+  }
+
+  /**
+  * a method to add rooms to link rooms to this one
+  * it does this by adding them to _linkedRooms
+  * 
+  * @param {string} direction the direction the other rooom is from this one
+  * @param {object} roomToLink the room that is in that direction
+  * @author Neil Bizzell
+  * @version 1.0
+  */
+  linkRoom(direction, roomToLink) {
+    this._linkedRooms[direction] = roomToLink;
+  }
+
+  /**
+   * a method to move the adventurer to a new room
+   * 
+   * @param {string} direction the direction in which to move
+   * @returns {object} the room moved to 
+   * @author Neil Bizzell
+   * @version 1.1
+   */
+  move(direction) {
+    if (direction in this._linkedRooms) {
+      return this._linkedRooms[direction];
+    } else {
+      alert("You can't go that way",);
+      return this;
+    }
+  }
+
+}
+
+class Character {
+  constructor(name, description, conversation) {
+    this._name = name,
+    this._description = description
+    this._conversation = conversation
+  }
+  set name(value) {
+    if (value.length < 4) {
+      alert("Name is too short.");
+      return;
+    }
+    this._name = value;
+  }
+
+  set description(value) {
+    if (value.length < 4) {
+      alert("Decription is too short.");
+      return;
+    }
+    this._description = value;
+  }
+
+  set conversation(value) {
+    if (value.length < 4) {
+      alert("conversation is too short.");
+      return;
+    }
+    this._conversation = value;
+  }
+  get name() {
+    return this._name;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get conversation() {
+    return this._conversation;
+  }
+  /**
+   * a method to produce friendly character description
+   * 
+   * @returns {string} description of the character
+   * @author Neil Bizzell
+   * @version 1.0
+   */
+  describe() {
+    return "You have met " + this._name + ", " + this._name + " is " + this._description;
+  }
+
+  /**
+   * a method to produce friendly conversation text
+   * 
+   * @returns {string} the conversation text
+   * @author Neil Bizzell
+   * @version 1.0
+   */
+  talk() {
+    return this._name + " says " + "'" + this._conversation + "'";
+  }
+}
+
+class Enemy extends Character{
+  constructor(name, description, conversation, weakness){
+   super(name, description, conversation)
+   this._weakness = weakness
+  }
+
+  fight(item){
+    if (item = this._weakness){
+      return true
+    } else {
+      return false
+    }
+  }
+} 
+
+
+const Kitchen = new Room("kitchen");
+Kitchen.description = "a long narrow room with worktops on either side and a large bench in the middle";
+const Lounge = new Room("lounge");
+Lounge.description = "a large room with two sofas and a large fire place";
+const GamesRoom = new Room("Games Room");
+GamesRoom.description = "a large room with a pool table at it's centre";
+const Hall = new Room("hall");
+Hall.description = "a grand entrance hall with large paintings around the walls";
+
+Kitchen.linkRoom("south", Lounge);
+Kitchen.linkRoom("east", Hall);
+Lounge.linkRoom("north", Kitchen);
+Lounge.linkRoom("east", GamesRoom);
+GamesRoom.linkRoom("west", Lounge);
+GamesRoom.linkRoom("north", Hall);
+Hall.linkRoom("south", GamesRoom);
+Hall.linkRoom("west", Kitchen);
+
+/**
+ * Subroutine to display information about the current room
+ * 
+ * @param {object} room the room to be displayed
+ * @author Neil Bizzell
+ * @version 1.0 
+ */
+ function displayRoomInfo(room) {
+  document.getElementById("textarea").innerHTML = room.describe()
+  document.getElementById("ui").focus();
+}
+
+function startGame() {
+  //set and display start room
+  currentRoom = Kitchen
+  displayRoomInfo(currentRoom);
+
+  //handle commands
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      command = document.getElementById("ui").value;
+      const directions = ["north", "south", "east", "west"]
+      if (directions.includes(command.toLowerCase())) {
+        currentRoom = currentRoom.move(command)
+        displayRoomInfo(currentRoom);
+        document.getElementById("ui").value = ""
+      } else {
+        document.getElementById("ui").value = ""
+        alert("that is not a valid command please try again")
+      }
+    }
+  });
+
+}
+
+startGame()
